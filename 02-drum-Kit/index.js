5b7b3c42f88be152f94e88b28c907033bc7ff9da
Garimanavlakha152005 <garimanavlakha152005@gmail.com>
2026-08-01 18:05:36 +0530

    Advanced DOM Manipulation

---
+++ 02-drum-Kit/index.js
@@ -0,0 +1,41 @@
+
+const w_drum_btn=document.querySelector('.w.drum');
+const a_drum_btn=document.querySelector('.a.drum');
+const s_drum_btn=document.querySelector('.s.drum');
+const d_drum_btn=document.querySelector('.d.drum');
+const j_drum_btn=document.querySelector('.j.drum');
+const k_drum_btn=document.querySelector('.k.drum');
+const l_drum_btn=document.querySelector('.l.drum');
+
+// w_drum_btn.addEventListener('click',()=>{
+//     const w_drum_note= new Audio('sounds/tom-1-w.mp3');
+//     w_drum_note.play();
+
+// });
+
+function playAudio(btn){
+    const audio= new Audio(`sounds/${btn}.mp3`);
+    audio.play();
+}
+
+w_drum_btn.addEventListener("click",()=>playAudio("w"));
+a_drum_btn.addEventListener("click",()=>playAudio("a"));
+s_drum_btn.addEventListener("click",()=>playAudio("s"));
+d_drum_btn.addEventListener("click",()=>playAudio("d"));
+j_drum_btn.addEventListener("click",()=>playAudio("j"));
+k_drum_btn.addEventListener("click",()=>playAudio("k"));
+l_drum_btn.addEventListener("click",()=>playAudio("l"));
+
+window.addEventListener("keydown",(event)=>{
+
+    let key = event.key.toLowerCase();
+
+    let validKeys=['w','a','s','d','j','k','l'];
+
+    if(validKeys.includes(key)){
+        playAudio(key);
+    }
+}
+)
+
+function buttonAnimation(key)
